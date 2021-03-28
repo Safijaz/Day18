49e1851d8ace82ac94b9ea04906f26b141a2d37e
Safijaz <79872266+Safijaz@users.noreply.github.com>
2021-03-28 22:38:51 +0200

    Add files via upload

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,13 +1,38 @@
-// Only change code below
-let shopingList = new Map([
-['groceries: Banana'],
-['groceries: Pineapple'],
-['groceries: Pear'],
-['groceries: Carrot'],
-['groceries: Apple'],
-]);
+// Only change code below this line 
+let shoppingList = new Map (
+    [
+        ["Banana",6],
+        ["Pineapple",1],
+        ["Pear",8],
+        ["Carrot",12],
+        ["Apple",11]
+    ]
+);
 
+let arrKey = [];
+let arrValue = [];
+
+for (var key of shoppingList.keys()) {
+    arrKey.push(key);
+}
+for (var key of arrKey) {
+    console.log("groceries:  "+key);
+}
+
+for (var value of shoppingList.values()) {
+    arrValue.push(value);
+}
+
+
+for (var value of arrValue) {
+    console.log("amount:  "+value);
+}
+
+
+for (var entry of shoppingList.entries()) {
+    console.log (entry);
+}
 
 
 //  Only change code above
-module.exports = { shoppingList, arrKez, arrValue};
+module.exports = {shoppingList,arrKey,arrValue};
