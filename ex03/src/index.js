b354b38f3df17d4bb3227679872980eec53a9ba5
Safijaz <79872266+Safijaz@users.noreply.github.com>
2021-03-28 22:37:22 +0200

    Add files via upload

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,15 +1,24 @@
 // Only change code below this line
-function*myGenerator(){
- yield*[1,2,3,4,5];
- yield*'Arena';
- yield*arguments;
-}
-const iterator= myGenerator(6,7,8);
- generatorArray=[];
- for (let i = 0; i < iterator.length; i += 1) 
-        generetorArray.push(myGenerator);
+function* myGenerator(num1,num2,num3) {
         
-          
+        yield* [1,2,3,4,5];
 
+        yield* 'Arena';
+        
+        yield num1;
+        yield num2;
+        yield num3;    
+    }
+    
+
+    var iterator = myGenerator(6,7,8);
+    var generatorArray = [];
+    
+    
+    for (var item of iterator) {
+     generatorArray.push(item);   
+    }
+
+    console.log(generatorArray);
 // Only change code above this line 
 module.exports={ generatorArray,myGenerator};
